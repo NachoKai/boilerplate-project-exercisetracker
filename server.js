b334bf1af8e665d1665c94bc3fcf7d236b5a6e8b
Nacho Caiafa <nachokai@outlook.com>
2020-11-21 11:32:18 -0300

    add post user

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
 const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-
 const cors = require("cors");
-
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const port = process.env.PORT || 3000;
+const app = express();
+const { Schema } = mongoose;
+
 mongoose.connect(process.env.DB_URI || "mongodb://localhost/exercise-track", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -13,36 +15,38 @@ mongoose.connect(process.env.DB_URI || "mongodb://localhost/exercise-track", {
 app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(bodyParser.json());
 
 app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-// Not found middleware
-app.use((req, res, next) => {
-  return next({ status: 404, message: "not found" });
-});
+// app.use((req, res, next) => {
+//   return next({ status: 404, message: "Not Found" });
+// });
 
-// Error Handling middleware
 app.use((err, req, res, next) => {
   let errCode, errMessage;
-
   if (err.errors) {
-    // mongoose validation error
-    errCode = 400; // bad request
+    errCode = 400;
     const keys = Object.keys(err.errors);
-    // report the first validation error
     errMessage = err.errors[keys[0]].message;
   } else {
-    // generic or custom error
     errCode = err.status || 500;
     errMessage = err.message || "Internal Server Error";
   }
   res.status(errCode).type("txt").send(errMessage);
 });
 
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+app.post("/api/exercise/new-user/", (req, res) => {
+  res.json({
+    userInput: req.body,
+  });
+});
+
+const listener = app.listen(port, () => {
+  console.log("Your app is listening on port " + port);
 });
